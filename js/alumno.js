421c87c9ad3ee65375c02e76c95a2d8c7fbf3f2e
nelsonFisi <aries_2221@hotmail.com>
2014-12-12 13:38:42 -0500

    Preparando el ambiente
    Agregando angular.js y lac creacion de index.html

---
+++ js/alumno.js
@@ -0,0 +1,20 @@
+function AlumnoController($scope){
+
+    $scope.dni = false;
+
+    $scope.usuario = {
+        nombre:"Ingeniero Nelson",
+        edad:25,
+        sexo:"no se"
+    }
+
+    $scope.alumnos = [
+        {nombre:"Nelson Coqchi Apaza",telefono:"3930542",curso:"Angular con Spring Framework"},
+        {nombre:"Ricardo Coqchi Apaza",telefono:"3930542",curso:"Java Web 1"},
+        {nombre:"Jorge Coqchi Apaza",telefono:"3930542",curso:"Java Web 2"},
+        {nombre:"Carmen Coqchi Apaza",telefono:"25814725",curso:"Informacion de las realidades"},
+        {nombre:"Sonia Coqchi Apaza",telefono:"123456789",curso:"Como hablar en publico"},
+        {nombre:"Liam Coqchi Meza",telefono:"1479585665",curso:"Comprarme mi carro en las estaciones"}
+    ];
+
+}
